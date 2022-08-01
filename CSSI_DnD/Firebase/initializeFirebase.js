144e21dbad1a15a312b95afff4e9d9aa61fa98f7
nklau <63122247+nklau@users.noreply.github.com>
2022-07-31 22:51:48 -0700

    fixed firebase config

--- CSSI_DnD/Firebase/initializeFirebase.js
+++ CSSI_DnD/Firebase/initializeFirebase.js
@@ -1,12 +1,3 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBWsaTGRve33eBNZKm87rJCYW0nfn2J3rc",
-  authDomain: "cssi-project-8bf38.firebaseapp.com",
-  projectId: "cssi-project-8bf38",
-  storageBucket: "cssi-project-8bf38.appspot.com",
-  messagingSenderId: "963989694906",
-  appId: "1:963989694906:web:916f0d06a18f4dea8fd583"
-};
-
 export const app = initializeApp(firebaseConfig);

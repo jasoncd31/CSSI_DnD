18bfdf361b27fa398ed0fc56f96debbeb590571e
nklau <63122247+nklau@users.noreply.github.com>
2022-08-01 18:47:49 -0700

    trying to call sign in function from index.html

--- CSSI_DnD/js/signIn.js
+++ CSSI_DnD/js/signIn.js
@@ -2,6 +2,9 @@ import { Auth } from '../Firebase/auth';
 export let auth = {};
 
 function signIn() {
+  console.log('script running');
   auth = new Auth();
   auth.signIn();
 }
+
+document.querySelector('#sign-in').addEventListener('click', signIn);

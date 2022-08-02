e5a85425f9d867de671881c8ed6676c9d63bddfd
Saad Salman <msalman2@lion.lmu.edu>
2022-08-01 19:24:15 -0700

    heyo

--- CSSI_DnD/js/signIn.js
+++ CSSI_DnD/js/signIn.js
@@ -1,10 +1,11 @@
-import { Auth } from '../Firebase/auth';
+import { Auth } from "../Firebase/auth";
 export let auth = {};
 
 function signIn() {
-  console.log('script running');
-  auth = new Auth();
-  auth.signIn();
+    console.log("function called");
+    auth = new Auth();
+    auth.signIn();
 }
 
-document.querySelector('#sign-in').addEventListener('click', signIn);
+console.log("script running");
+document.querySelector("#sign-in").addEventListener("click", signIn);

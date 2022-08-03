801da76c427cfaae06714c5a32d11b66cf8e2c5a
nklau <63122247+nklau@users.noreply.github.com>
2022-08-02 21:49:07 -0700

    moved signIn function to module

--- CSSI_DnD/Firebase/auth.js
+++ CSSI_DnD/Firebase/auth.js
@@ -1,42 +1,60 @@
 import { getAuth, signOut, EmailAuthProvider, GoogleAuthProvider, PhoneAuthProvider } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js';
 // import { auth } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase.js';
 // import { AuthUI } from 'https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js'; // also causes error - is this not a module?
-import * as firebase from 'https://www.gstatic.com/firebasejs/8.10.1/firebase.js';
-import * as firebaseui from 'https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js';
+// import * as firebase from 'https://www.gstatic.com/firebasejs/8.10.1/firebase.js';
+// import * as firebaseui from 'https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js';
 
-export class Auth {
-
-  constructor() {
-    this.authUI = new firebaseui.auth.AuthUI(firebase.auth());
-    this.displayName = null; // display name will be null if user is not signed in
-
-    this.uiConfig = {
-      signInOptions: [
-        EmailAuthProvider.PROVIDER_ID,
-        PhoneAuthProvider.PROVIDER_ID,
-        {
-          provider: GoogleAuthProvider.PROVIDER_ID,
-          recaptchaParameters: {
-            size: 'invisible'
-          }
+export function signIn() {
+  const authUI = new firebaseui.auth.AuthUI(firebase.auth());
+  const uiConfig = {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      {
+        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        recaptchaParameters: {
+          size: 'invisible'
         }
-      ]
-    };
-  }
-
-  signIn() {
-    this.authUI.start('#firebaseui-auth-container', uiConfig);
-    this.displayName = getAuth().currentUser.displayName;
-  }
-
-  signOut() {
-    if (window.confirm('Are you sure you want to sign out?')) {
-      signOut(getAuth()).then(() => {
-        window.displayName = null;
-      }).catch((error) => {
-        alert(`Error signing out. Error code: ${error}`);
-      })
-    }
-  }
-
+      }
+    ]
+  };
+  authUI.start('#firebaseui-auth-container', uiConfig);
+  // window.displayName = getAuth().currentUser.displayName;
 }
+
+// export class Auth {
+
+//   constructor() {
+//     this.authUI = new firebaseui.auth.AuthUI(firebase.auth());
+//     this.displayName = null; // display name will be null if user is not signed in
+
+//     this.uiConfig = {
+//       signInOptions: [
+//         EmailAuthProvider.PROVIDER_ID,
+//         PhoneAuthProvider.PROVIDER_ID,
+//         {
+//           provider: GoogleAuthProvider.PROVIDER_ID,
+//           recaptchaParameters: {
+//             size: 'invisible'
+//           }
+//         }
+//       ]
+//     };
+//   }
+
+//   signIn() {
+//     this.authUI.start('#firebaseui-auth-container', uiConfig);
+//     this.displayName = getAuth().currentUser.displayName;
+//   }
+
+//   signOut() {
+//     if (window.confirm('Are you sure you want to sign out?')) {
+//       signOut(getAuth()).then(() => {
+//         window.displayName = null;
+//       }).catch((error) => {
+//         alert(`Error signing out. Error code: ${error}`);
+//       })
+//     }
+//   }
+
+// }
--- CSSI_DnD/js/signIn.js
+++ CSSI_DnD/js/signIn.js
@@ -1,17 +1,3 @@
-const authUI = new firebaseui.auth.AuthUI(firebase.auth());
-const uiConfig = {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-    {
-      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      recaptchaParameters: {
-        size: 'invisible'
-      }
-    }
-  ]
-};
-document.querySelector('#sign-in').addEventListener('click', () => {
-  authUI.start('#firebaseui-auth-container', uiConfig);
-  window.displayName = getAuth().currentUser.displayName;
-})
+import { signIn } from '../Firebase/auth.js';
+
+document.querySelector('#sign-in').addEventListener('click', signIn);

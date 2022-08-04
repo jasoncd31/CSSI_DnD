e9c6ca4cd53ffc2d88bda9aebc173095e029b516
nklau <63122247+nklau@users.noreply.github.com>
2022-08-03 18:18:44 -0700

    fixed firebase methods to not be async anymore, changed document name to character name

--- CSSI_DnD/js/isSignedIn.js
+++
@@ -1,6 +0,0 @@
-import { getDisplayName } from '../Firebase/initializeFirebase.js';
-
-/// Return true if there is an user currently signed in, false otherwise
-function isSignedIn() {
-  return getDisplayName() != null;
-}
